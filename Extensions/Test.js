7f1d2828839cd756b87a9be6fb42bad21c590d80
Summer <106570213+Webzzenn@users.noreply.github.com>
2026-02-23 19:18:45 +0000

    test

--- Extensions/Test.js
+++ Extensions/Test.js
@@ -1,1 +1,30 @@
-Math.SQRT2
+(function (Scratch) {
+  'use strict';
+
+  class Extension {
+    getInfo() {
+      return {
+        id: 'testbranch',
+        name: "branchtest",
+        color1: '#d4bd56',
+        color2: '#a79543',
+        color3: '#f6e9ae',
+        blocks: [
+          {
+            opcode: 'branch',
+            blockType: Scratch.BlockType.COMMAND,
+            disableMonitor: true,
+            text: "my test branch",
+            branchCount: 1,
+          }
+        ]
+      };
+    }
+
+    branch(argsm, util) {
+      util.startBranch(1, false);
+    }
+  }
+
+  Scratch.extensions.register(new Extension());
+})(Scratch);
